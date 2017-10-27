1a16fc029a538f5bbd26d53735159482fb85cf1d
Darwin757 <Darwin.rocker@gmail.com>
2017-10-27 15:04:01 +0200

    nothign

--- src/test/java/com/example/Westeros/Kingdoms/KingdomServiceTest.java
+++ src/test/java/com/example/Westeros/Kingdoms/KingdomServiceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.Westeros.WesterosApplication;
 import com.example.Westeros.Kingdoms.Kingdom;
